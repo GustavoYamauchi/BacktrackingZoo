9a5e242e063a9305a5a7fb00e22ba1057f1935ba
unknown <34483777+MarcoABN@users.noreply.github.com>
2021-03-31 19:57:41 -0300

    feat: constraints

--- constraint.py
+++ constraint.py
@@ -1,4 +1,5 @@
 import numpy as np
+from functools import reduce
 
 class Constraint():
     def __init__(self, variables):
@@ -18,12 +19,24 @@ class NotEqual(Constraint):
         domains = [variableToDomain[variable] for variable in self.variables]
         return len(set(domains)) == len(domains)
 
+class Equal(Constraint):
+    def __init__(self, v1, v2):
+        super().__init__([v1, v2])
+        self.variables = [v1, v2]
 
+    def isSatisfied(self, variableToDomain):
+        if not all(variable in variableToDomain for variable in self.variables):
+            return True
+        domains = [variableToDomain[variable] for variable in self.variables]
+        return len(set(domains)) == 1
 
+class NotAdjacent(Constraint):
+    def __init__(self, v1, v2):
+        super().__init__([v1, v2])
+        self.variables = [v1, v2]
 
-        # # Não analise se todos os estados estiverem atribu;idos
-        # if not all(variavel in atribuicao for variavel in self.variaveis):
-        #   return True
-        # # cores de estados vizinhos não podem ser igual
-        # valores = [atribuicao[variavel] for variavel in self.variaveis]
-        # return len(set(valores)) == len(valores)
+    def isSatisfied(self, variableToDomain):
+        if not all(variable in variableToDomain for variable in self.variables):
+            return True
+        domains = [variableToDomain[variable] for variable in self.variables]
+        return abs(reduce(lambda total, element: total - variableToDomain[element])) > 1
